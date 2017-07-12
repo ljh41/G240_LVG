bac3927a7cf89f422b2777809cd693351cad1258
starform <starform@liujh-MacBook-Pro.local>
2017-07-12 19:36:19 +0800

    7.12

--- github.sh
+++ github.sh
@@ -1,6 +1,6 @@
 #git rm -r --cached .
 git add .
-git commit -m "1.12 "
-git remote add 1.12 https://github.com/ljh41/G240_LVG.git
-git push -u 1.12 master
+git commit -m "7.12 "
+git remote add 7.12 https://github.com/ljh41/G240_LVG.git
+git push -u 7.12 master
 
