423e32ce26400ee2fbd1fa685bc9489ca89efeae
starform <starform@liujh-MacBook-Pro.local>
2017-11-30 16:08:18 +0800

    11.30

--- github.sh
+++ github.sh
@@ -1,7 +1,7 @@
 #git rm -r --cached .
 #git init
 git add .
-git commit -m "11.29 "
-git remote add 11.29 https://github.com/ljh41/G240_LVG.git
-git push -u 11.29 master
+git commit -m "11.30 "
+git remote add 11.30 https://github.com/ljh41/G240_LVG.git
+git push -u 11.30 master
 
