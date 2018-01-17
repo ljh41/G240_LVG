47af25dc9052f00ecbe49372ccc0312d9c263f50
starform <starform@liujh-MacBook-Pro.local>
2018-01-17 10:17:18 +0800

    1.17

--- github.sh
+++ github.sh
@@ -1,7 +1,7 @@
 #git rm -r --cached .
 #git init
 git add .
-git commit -m "1.1 "
-git remote add 1.1 https://github.com/ljh41/G240_LVG.git
-git push -u 1.1 master
+git commit -m "1.17 "
+git remote add 1.17 https://github.com/ljh41/G240_LVG.git
+git push -u 1.17 master
 
