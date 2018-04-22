b6cbc8b0961f8785744e0cd8d5331c5809a564f2
starform <starform@liujh-MacBook-Pro.local>
2018-04-22 19:37:01 +0800

    4.22

--- github.sh
+++ github.sh
@@ -1,7 +1,7 @@
 #git rm -r --cached .
 #git init
 git add .
-git commit -m "4.5 "
-git remote add 4.5 https://github.com/ljh41/G240_LVG.git
-git push -u 4.5 master
+git commit -m "4.22 "
+git remote add 4.22 https://github.com/ljh41/G240_LVG.git
+git push -u 4.22 master
 
