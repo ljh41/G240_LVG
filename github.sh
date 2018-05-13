bfa43294352bc5f1496d009614e6d43f23b8b237
starform <starform@liujh-MacBook-Pro.local>
2018-05-13 09:19:49 +0800

    last

--- github.sh
+++ github.sh
@@ -1,7 +1,7 @@
 #git rm -r --cached .
 #git init
 git add .
-git commit -m "5.1 "
-git remote add 5.1 https://github.com/ljh41/G240_LVG.git
-git push -u 5.1 master
+git commit -m "last "
+git remote add last https://github.com/ljh41/G240_LVG.git
+git push -u last master
 
